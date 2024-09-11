6eeb5ade3a6e79568e000fc4dadff8cea0e7e8bd
Matthew <benderm@msoe.edu>
2024-09-11 16:26:00 -0500

    Completed DieNotRolledException.java

---
+++ src/benderm/DieNotRolledException.java
@@ -0,0 +1,14 @@
+/*
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * DieNotRolledException class
+ * Name: Matthew Bender
+ * Last Updated: 9/11/2024
+ */
+package benderm;
+
+/**
+ * Exception that's raised when the Die class is used improperly.
+ */
+public class DieNotRolledException extends Exception {
+}
